b3a6437daf67055743101f3d248ff2e26f105e47
Trung <trung.duong@parcelperform.com>
2022-07-20 17:31:54 +0700

    fetch without error classification

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+
+import axios from "axios";
+import React from "react";
+
+const URL_200 = "https://pokeapi.co/api/v2/pokemon/ditto";
+const URL_401 = "https://api.spoonacular.com/recipes/complexSearch";
+const URL_404 = "https://pokeapi.co/api/v2/pokemon/notarealpokemon";
+
+// even though status code of response is 401, which should be an APIError, the catch block won't run
+// because fetch does not recognize non-2xx status code response as error
+function fetchWithoutErrorClassification() {
+  fetch(URL_401)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log("[SUCCESS]", json);
+    })
+    .catch((error) => {
+      console.log("[ERROR]", error);
+    });
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  React.useEffect(() => {
+    console.clear();
+    fetchWithoutErrorClassification();
+  }, []);
+  return <div style={{ padding: "100px 300px" }}>Error Handling Demo</div>;
 }
 
 export default App;
